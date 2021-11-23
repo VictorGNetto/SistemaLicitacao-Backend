39fa07b99af146d80e66100a30c63d0405b38528
VictorGNetto <victornetto@discente.ufg.br>
2021-11-23 15:13:33 -0300

    adicionado clonagem de Documentos Base

---
+++ clonar-documento-base.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once "id-generator.php";
+require_once "db-config.php";
+require_once "utils.php";
+
+function clone_item($itemID, $conn) {
+    $newItemID = get_unused_itemID($conn);
+
+    try {
+        $sql = "SELECT dados FROM itens WHERE itemID = '" . $itemID . "'";
+        $result = $conn->query($sql);
+        $row = $result->fetch();
+        $data = $row["dados"];
+    } catch (PDOException $e) {
+        die ("ERROR: Could not able to execute $sql. " . $e->getMessage());
+    }
+
+    try {
+        // Prepara a declaração
+        $sql = "INSERT INTO itens (itemID, dados) VALUES (:itemID, :dados)";
+        $stmt = $conn->prepare($sql);
+
+        // Vincula parâmetros à declaração
+        $stmt->bindParam(":itemID", $newItemID, PDO::PARAM_STR);
+        $stmt->bindParam(":dados", $data, PDO::PARAM_STR);
+
+        $stmt->execute();
+    } catch (PDOException $e) {
+        die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+    }
+
+    return $newItemID;
+}
+
+$baseDocumentID = $_GET["documentoBaseID"];  // ID do Documento Base original
+
+$newBaseDocumentID = n_letters_id(3);        // ID do nono Documento Base
+$baseDocumentName = "";
+$sections = "";
+
+// Encontra um ID ainda não usado para o Documento Base
+while (true) {
+    try {
+        $sql = "SELECT * FROM documentos_base WHERE documentoBaseID = '" . $newBaseDocumentID . "'";
+        $result = $conn->query($sql);
+        if ($result->rowCount() === 0) break;
+        $newBaseDocumentID = n_letters_id(3);
+    } catch (PDOException $e) {
+        die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+    }
+}
+
+// Clona os dados do Documento Base para o novo Documento Base
+try {
+    $sql = "SELECT nomeDocumentoBase, secoes FROM documentos_base WHERE documentoBaseID = '" . $baseDocumentID . "'";
+    $result = $conn->query($sql);
+    $row = $result->fetch();
+    $baseDocumentName = $row["nomeDocumentoBase"] . " [Cópia]";
+    $sections = json_decode($row["secoes"]);
+    
+    foreach ($sections as $section) {
+        $items = $section->itens;
+        foreach ($items as $item) {
+            $newItemID = clone_item($item->itemID, $conn);
+            $item->itemID = $newItemID;
+        }
+    }
+
+    $sections = json_encode($sections, JSON_UNESCAPED_UNICODE);
+} catch (PDOException $e) {
+    die ("ERROR: Could not able to execute $sql. " . $e->getMessage());
+}
+
+try {
+    $sql = "INSERT INTO documentos_base (documentoBaseID, nomeDocumentoBase, secoes) VALUES ('" .
+            $newBaseDocumentID . "', '" .
+            $baseDocumentName . "', '" .
+            $sections . "')";
+    
+    $conn->exec($sql);
+} catch (PDOException $e) {
+    die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+}
+
+unset($conn);
+
+echo json_encode(["documentoBaseID" => $newBaseDocumentID]);
--- obter-info-sessao.php
+++ obter-info-sessao.php
@@ -4,13 +4,13 @@ $hash_sessao = $_GET["sessaoID"];   // Sessão
 $hash_sistema = "bc1b07991f7a49331bf9c45b5baa3ec4";  // Sistema
 $ip = "10.232.94.64";
 
-// // if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-// //     $ip = $_SERVER['HTTP_CLIENT_IP'];
-// // } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-// //     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-// // } else {
-// //     $ip = $_SERVER['REMOTE_ADDR'];
-// // }
+// if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+//     $ip = $_SERVER['HTTP_CLIENT_IP'];
+// } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+//     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+// } else {
+//     $ip = $_SERVER['REMOTE_ADDR'];
+// }
 
 $url = 'http://10.6.56.199/portal/servicos/serverbyserver/get-usuario-sessao.php';
 $data = array('sisID' => $hash_sistema, 'hash' => $hash_sessao, 'ip' => $ip);
